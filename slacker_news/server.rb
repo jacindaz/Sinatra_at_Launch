f89f50bb1d02d9fbe0dfc054094e3567facb1f92
Jacinda Zhong <jacindaz@gmail.com>
2014-05-22 21:05:23 -0400

    Adds user input from form to csv
     - using params hash, writes to articles.csv
     - uses a form, and grabs the value stores in the <input> or <textarea>
       tag name (name="title", for example) and saves into params hash
     - then access the value is params hash to save into csv

--- slacker_news/server.rb
+++ slacker_news/server.rb
@@ -1,15 +1,47 @@
 require 'sinatra'
 require 'rubygems'
-require 'csv'
 require 'pry'
+require 'csv'
 
 
 #METHODS--------------------------------------------------------------
+def load_csv(file_name)
+  articles = []
+  CSV.foreach(file_name, headers: true, header_converters: :symbol) do |article_hash|
+    articles << article_hash.to_hash
+  end
+  articles
+end
 
 
 #ROUTES AND VIEWS------------------------------------------------------
 get '/' do
   @title = "Slacker News"
+  @articles = load_csv('articles.csv')
+
+  #binding.pry
 
   erb :index
+end
+
+get '/new_article' do
+  @title = "Submit a new article"
+
+  erb :new_article
+end
+
+post '/article' do
+  @title = params[:title]
+  @url = params[:url]
+  @description = params[:description]
+
+  #binding.pry
+
+  File.open('articles.csv', 'a') do |file|
+    file.puts("#{@title}, #{@url}, #{@description}")
+  end
+
+
+
+  redirect '/'
 end
